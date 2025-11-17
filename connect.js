a3d1b84bb97ec1a7e22f0a116b500bde8cbbc89a
Sid The Hacker <131943405+sid238@users.noreply.github.com>
2025-11-17 22:06:43 +0530

    Update import from '@yupra/baileys' to 'baileys-york'

--- connect.js
+++ connect.js
@@ -17,7 +17,7 @@ const {
     fetchLatestBaileysVersion,
     downloadContentFromMessage,
     jidNormalizedUser
-} = require('@yupra/baileys');
+} = require('baileys-york');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
